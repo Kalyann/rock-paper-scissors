f42c28b1d66ae281cc7b19aa06e3cd51798ce53c
Kalyan Lahkar <klahkar25@gmail.com>
2016-02-13 23:01:37 +0530

    Create rps.js

---
+++ rps.js
@@ -0,0 +1,42 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+var compare = function(choice1, choice2){ 
+ var s;
+ if( choice1 === choice2){
+ s = "The result is a tie!";
+ }
+ else if( choice1 === "rock"){
+     if( choice2 === "scissors"){
+         s="rock wins";
+     }
+     else if( choice2 === "paper"){
+         s="paper wins";
+     }
+ }
+ else if( choice1 === "paper"){
+     if( choice2 === "rock"){
+         s="paper wins";
+     }
+     else if( choice2 === "scissors"){
+         s="scissors wins";
+     }
+ }
+ else if( choice1 === "scissors"){
+     if( choice2 === "rock"){
+         s="rock wins";
+     }
+     else if( choice2 === "paper"){
+         s="scissors wins";
+     }
+ }
+return s;
+}
+compare(userChoice, computerChoice);
